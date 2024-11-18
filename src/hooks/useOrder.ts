f73cc6ee0281cdacc5ca9dd3eeb6ccd7515e95af
Luis-Sarmiento <luissarmientomarquez@gmail.com>
2024-11-17 21:09:11 -0500

    Creando el State order y la función addItem

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,11 +1,25 @@
 import { useState } from "react"
-import { UseOrder } from "../types"
+import { MenuItem, UseOrder } from "../types"
 
 export default function useOrder() {
+    
     const [order, setOrder] = useState<UseOrder[]>([])
-    const addItem = () => {
-        console.log('agregando...')
+    
+    const addItem = (item: MenuItem) => {
+        
+        const itemExists = order.find(orderItem => orderItem.id === item.id)
+        if(itemExists){
+            const updatedOrder = order.map(orderItem => orderItem.id === item.id ?
+                {...orderItem, quantity: orderItem.quantity + 1} :
+                orderItem
+            )
+            setOrder(updatedOrder)
+        }else{
+            const newItem: UseOrder = {...item, quantity: 1}
+            setOrder([...order, newItem]) 
+        }
     }
+    console.log(order)
 
     return{
         addItem
